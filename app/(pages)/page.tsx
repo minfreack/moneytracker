71ac60c9922fb2843a49a0fd4d0c28b6dfe07f96
luis <neto2racas@gmail.com>
2024-04-17 19:44:33 -0600

    Route change

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,33 +1,31 @@
 'use client'
 import { title, subtitle } from "@/app/components/primitives";
 import { AuthContext } from "@/context/auth";
-import { redirect } from 'next/navigation'
-import { useContext, useEffect } from "react";
+import {redirect, usePathname, useRouter} from 'next/navigation'
+import {useContext, useEffect, useState} from "react";
 
 
 export default function Home() {
-    const {userAuth} = useContext(AuthContext)
-		
+
+	const { userAuth } = useContext(AuthContext);
+	const router = useRouter();
+	const pathname = usePathname();
+
 	useEffect(() => {
-		if(userAuth?.auth?.auth){
-			redirect('/dashboard')
-		}
-	},[])
+		const checkAuthentication = async () => {
+			if (!userAuth?.auth?.auth) {
+					await router.push('/login');
+					return;
+			}
+			if (userAuth?.auth?.auth) {
+				await router.push('/dashboard');
+				return;
+			}
+		};
+
+		checkAuthentication();
+	}, [userAuth, router]);
 
 
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
-		</section>
-	);
+	return
 }
